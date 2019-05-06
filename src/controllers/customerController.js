7c65d707574bb2ef99f6229871115be101c025ab
Pawel Buda <p-buda@users.noreply.github.com>
2019-05-06 16:55:57 -0500

    Add routes for orders

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -19,15 +19,52 @@ exports.addCust = function(req, res) {
 }
 
 exports.showCust = function(req, res) {
-    res.send('Calling showCust function');
+    // res.send('Calling showCust function');
+    if(!req.query.email) {
+        return res.status(400).send('Missing URL parameter.');
+    }
+    Customer.findOne({
+        email: req.query.email
+    })
+        .then((doc) => {
+            res.json(doc);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
 }
 
 exports.changeCust = function(req, res) {
-    res.send('Calling changeCust function');
+    // res.send('Calling changeCust function');
+    if(!req.query.email) {
+        return res.status(400).send('Missing URL parameter.');
+    }
+    Customer.findOneAndUpdate({
+        email: req.query.email
+    }, req.body, {new: true})
+    .then((doc) => {
+        res.json(doc);
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    });
+
 }
 
 exports.deleteCust = function(req, res) {
-    res.send('Calling deleteCust function');
+    // res.send('Calling deleteCust function');
+    if(!req.query.email) {
+        return res.status(400).send('Missing URL parameter.');
+    }
+    Customer.findOneAndRemove({
+        email: req.query.email
+    })
+    .then((doc) => {
+        res.json(doc);
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    });
 }
 
 
